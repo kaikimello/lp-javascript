69ddaf1b85290168197777c5f3b8e9297f1ab420
Kaiki Mello <kaiki.mello@gmail.com>
2024-01-13 19:21:12 -0300

    Adicionando número de tentativas ao jogo

--- game-js-main/app.js
+++ game-js-main/app.js
@@ -2,19 +2,22 @@ alert('Boas vindas ao jogo do número secreto ');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
 let chute;
+let tentativas = 1;
 
 // Enquanto o chute não for igual ao número secreto 
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
     // Se o chute for igual a número secreto 
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
     } else {
         if (chute > numeroSecreto){
             alert(`O número secreto é menor que ${chute}`);
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
+        // tentativas = tentativas + 1;
+        tentativas++;
     }
 }
 
